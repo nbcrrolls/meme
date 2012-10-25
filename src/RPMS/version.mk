3396655bc2d58471344d0040359aa3059b8c0d9f
Nadya Williams <nadya@sdsc.edu>
2012-10-25 21:51:01 +0000

    add HTML-Template perl package

--- src/RPMS/version.mk
+++ src/RPMS/version.mk
@@ -3,3 +3,5 @@ CLASS_STD       = opt-perl-Class-Std
 CLASS_STD_FAST  = opt-perl-Class-Std-Fast
 SOAP_WSDL       = opt-perl-SOAP-WSDL
 TimeDate        = opt-perl-TimeDate
+HTML_Template   = opt-perl-HTML-Template
+
