2ec16ee56fd1e2f0df0aa528680a36039a740afb
nadyawilliams <nadya@sdsc.edu>
2013-09-30 10:57:18 -0700

    update motif database to v10

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 #
 # Run before making a roll. 
 # Create and install (in src/RPMS) prereq perl modules rpms
+# Needed during meme build for creating db csv files
 #
 
 IGNORE="--default-ignorelist --ignorelist `pwd`/modules-ignore"
@@ -18,44 +19,37 @@ fi
 cd $EXTRA
 
 doClassStd () {
-/opt/perl/bin/cpan2dist --format CPANPLUS::Dist::Rocks $IGNORE  Class::Std
+/opt/perl/bin/cpan2dist  --force --format CPANPLUS::Dist::Rocks $IGNORE  Class::Std
 MOD=`ls Class-Std/noarch/*.rpm`
 rpm -i $MOD
 cp $MOD ../src/RPMS
 }
 
 doClassStdFast () {
-/opt/perl/bin/cpan2dist --format CPANPLUS::Dist::Rocks $IGNORE  Class::Std::Fast
+/opt/perl/bin/cpan2dist  --force --format CPANPLUS::Dist::Rocks $IGNORE  Class::Std::Fast
 MOD=`ls Class-Std-Fast/noarch/*.rpm`
 rpm -i $MOD
 cp $MOD ../src/RPMS
 }
 
 doTimeDate () {
-/opt/perl/bin/cpan2dist --format CPANPLUS::Dist::Rocks $IGNORE Date::Format
+/opt/perl/bin/cpan2dist  --force --format CPANPLUS::Dist::Rocks $IGNORE Date::Format
 MOD=`ls TimeDate/noarch/*.rpm`
 rpm -i $MOD
 cp $MOD ../src/RPMS
 }
 
 doSoapWsdl () {
-/opt/perl/bin/cpan2dist --format CPANPLUS::Dist::Rocks $IGNORE SOAP::WSDL
+/opt/perl/bin/cpan2dist  --force  --format CPANPLUS::Dist::Rocks $IGNORE SOAP::WSDL
 MOD=`ls SOAP-WSDL/noarch/*.rpm`
 rpm -i $MOD
 cp $MOD ../src/RPMS
 }
 
 doHTMLTemplate () {
-/opt/perl/bin/cpan2dist --format CPANPLUS::Dist::Rocks $IGNORE HTML::Template
+/opt/perl/bin/cpan2dist --force --format CPANPLUS::Dist::Rocks $IGNORE HTML::Template
 MOD=`ls HTML-Template/noarch/*.rpm`
 rpm -i $MOD
-cp $MOD ../src/RPMS
-}
-
-doXMLParser () {
-/opt/perl/bin/cpan2dist --force --format CPANPLUS::Dist::Rocks $IGNORE XML::Parser
-MOD=`ls XML-Parser/noarch/*.rpm`
-#rpm -i $MOD
 cp $MOD ../src/RPMS
 }
 
@@ -65,5 +59,4 @@ doClassStdFast
 doTimeDate
 doSoapWsdl
 doHTMLTemplate
-#doXMLParser
 
