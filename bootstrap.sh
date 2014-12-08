ebbea34ccffb492f1f4a20fcee1501ce2bffa40b
nadyawilliams <nadya@sdsc.edu>
2014-12-08 14:23:24 -0800

    delete most extra perl rpms, only HTML-Template is needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,34 +28,6 @@ echo "meme `hostname -s`:/export/&" >> /etc/auto.share
 compile_and_install meme-db
 }
 
-doClassStd () {
-/opt/perl/bin/cpan2dist  --force --format CPANPLUS::Dist::Rocks $IGNORE  Class::Std
-MOD=`ls Class-Std/noarch/*.rpm`
-rpm -i $MOD
-cp $MOD ../src/RPMS
-}
-
-doClassStdFast () {
-/opt/perl/bin/cpan2dist  --force --format CPANPLUS::Dist::Rocks $IGNORE  Class::Std::Fast
-MOD=`ls Class-Std-Fast/noarch/*.rpm`
-rpm -i $MOD
-cp $MOD ../src/RPMS
-}
-
-doTimeDate () {
-/opt/perl/bin/cpan2dist  --force --format CPANPLUS::Dist::Rocks $IGNORE Date::Format
-MOD=`ls TimeDate/noarch/*.rpm`
-rpm -i $MOD
-cp $MOD ../src/RPMS
-}
-
-doSoapWsdl () {
-/opt/perl/bin/cpan2dist  --force  --format CPANPLUS::Dist::Rocks $IGNORE SOAP::WSDL
-MOD=`ls SOAP-WSDL/noarch/*.rpm`
-rpm -i $MOD
-cp $MOD ../src/RPMS
-}
-
 doHTMLTemplate () {
 /opt/perl/bin/cpan2dist --force --format CPANPLUS::Dist::Rocks $IGNORE HTML::Template
 MOD=`ls HTML-Template/noarch/*.rpm`
@@ -63,20 +35,7 @@ rpm -i $MOD
 cp $MOD ../src/RPMS
 }
 
-doFTPAutoReconnect () {
-/opt/perl/bin/cpan2dist --force --format CPANPLUS::Dist::Rocks $IGNORE Net::FTP::AutoReconnect
-MOD=`ls Net-FTP-AutoReconnect/noarch/*.rpm`
-rpm -i $MOD
-cp $MOD ../src/RPMS
-}
-
 ### main 
-doClassStd
-doClassStdFast
-doTimeDate
-doSoapWsdl
 doHTMLTemplate
-doFTPAutoReconnect
-
 doDBs
 
--- src/RPMS/version.mk
+++ src/RPMS/version.mk
@@ -1,8 +1,3 @@
 NAME    	= meme-perl-prereq
-CLASS_STD       = opt-perl-Class-Std
-CLASS_STD_FAST  = opt-perl-Class-Std-Fast
-SOAP_WSDL       = opt-perl-SOAP-WSDL
-TimeDate        = opt-perl-TimeDate
 HTML_Template   = opt-perl-HTML-Template
-Net_FTP         = opt-perl-Net-FTP-AutoReconnect
 
